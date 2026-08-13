1f4f4c6b59e7df018399edfb3e7d1529133ea1c6
sanbrita-lab <sanbri1010@gmail.com>
2026-08-13 20:39:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://lsflbpscljvygperoyij.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_vWb2bFOiBbxTaUPxl7nkGQ_L36dzJzd";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
